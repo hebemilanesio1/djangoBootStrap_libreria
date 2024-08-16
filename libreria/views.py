e9297d809344cd75071c5d16819ce80d3e50f67b
hebem <hebemilanesio1@gmail.com>
2024-08-16 11:39:38 -0300

    Primera Versión Sistema Librería

---
+++ libreria/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
+
+# Create your views here.
+
+def inicio(request):
+    #Imprime un texto html
+    return render(request, 'paginas/inicio.html')
+
+def nosotros(request):
+    #Busca un archivo nosotros.html donde accede directamente en templates
+    return render(request, 'paginas/nosotros.html')
+
+def libros(request):
+    libros = Libro.objects.all()
+    print(libros)
+    return render(request, 'libros/index.html', {'libros': libros})
+
+def crear(request):
+    formulario = LibroForm(request.POST or None,request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')    
+    return render(request, 'libros/crear.html', {'formulario': formulario})
+
+def editar(request,id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/editar.html',{'formulario': formulario})
+
+def eliminar(request,id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
